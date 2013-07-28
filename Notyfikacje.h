ec171355de201ff1ea290638c002a59bb246d583
Krzysztof Grochocki <beherit@users.noreply.github.com>
2013-07-28 12:07:45 +0200

    * Pierwsza publiczna wersja.

---
+++ Notyfikacje.h
@@ -0,0 +1,14 @@
+#ifndef Notyfikacje_H
+#define Notyfikacje_H
+
+#define  AQQ_CONTROLS_TOOLBAR			L"AQQ/Controls/Toolbar/"
+#define  AQQ_CONTROLS_CREATEBUTTON		L"/CreateButton"
+#define  AQQ_CONTROLS_DESTROYBUTTON		L"/DestroyButton"
+
+#define  AQQ_CONTROLS_CREATEPOPUPMENU		L"AQQ/Controls/CreatePopUpMenu"
+#define  AQQ_CONTROLS_CREATEPOPUPMENUITEM	L"AQQ/Controls/CreatePopUpMenuItem"
+#define  AQQ_CONTROLS_DESTROYPOPUPMENU		L"AQQ/Controls/DestroyPopUpMenu"
+#define  AQQ_CONTROLS_DESTROYPOPUPMENUITEM	L"AQQ/Controls/DestroyPopUpMenuItem"
+
+#endif
+ 
